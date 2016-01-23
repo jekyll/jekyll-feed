3fe1b48e74f11dc804246d3e2734120f1d446c77
Pat Hawks <pat@pathawks.com>
2016-01-23 11:17:35 -0800

    Add note about StripWhitespace

--- lib/jekyll/strip_whitespace.rb
+++ lib/jekyll/strip_whitespace.rb
@@ -1,3 +1,7 @@
+# The `strip` filter was added to Liquid in version 3
+# Jekyll did not require Liquid 3 until Jekyll 3.0.0
+# This shim is needed for compatibility with Jekyll 2.x
+
 module Jekyll
   module StripWhitespace
     def strip(input)
