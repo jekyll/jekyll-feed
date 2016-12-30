bf728c321d715f8bb1673e43fb3c848fe736fa6d
Ben Balter <ben.balter@github.com>
2016-12-30 13:30:06 -0500

    use absolute_url to generate the feed_meta url

--- lib/jekyll/feed_meta_tag.rb
+++ lib/jekyll/feed_meta_tag.rb
@@ -1,5 +1,8 @@
 module Jekyll
   class FeedMetaTag < Liquid::Tag
+    # Use Jekyll's native relative_url filter
+    include Jekyll::Filters::URLFilters
+
     def render(context)
       @context = context
       attrs    = attributes.map { |k, v| %(#{k}="#{v}") }.join(" ")
@@ -16,7 +19,7 @@ module Jekyll
       {
         :type  => "application/atom+xml",
         :rel   => "alternate",
-        :href  => "#{url}/#{path}",
+        :href  => absolute_url(path),
         :title => title
       }.keep_if { |_, v| v }
     end
@@ -26,14 +29,6 @@ module Jekyll
         config["feed"]["path"]
       else
         "feed.xml"
-      end
-    end
-
-    def url
-      if config["url"]
-        URI.join(config["url"], config["baseurl"])
-      elsif config["github"] && config["github"]["url"]
-        config["github"]["url"]
       end
     end
 
