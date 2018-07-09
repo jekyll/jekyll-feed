004bd3bf93114462a233d0faab7c999518496280
Pat Hawks <pat@pathawks.com>
2018-07-09 11:28:11 -0500

    Fix most Rubocop offenses

--- lib/jekyll-feed/generator.rb
+++ lib/jekyll-feed/generator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'digest'
+require "digest"
 
 module JekyllFeed
   class Generator < Jekyll::Generator
@@ -13,7 +13,7 @@ module JekyllFeed
       return if file_exists?(feed_path)
 
       # All feed documents use the same template, so just read it once.
-      feed_source = File.read(feed_source_path).gsub(MINIFY_REGEX, "")
+      @feed_source = File.read(feed_source_path).gsub(MINIFY_REGEX, "")
 
       # Any archive pages should link to the current feed, so set up that page
       # early so we can ask it for its URL later.
@@ -23,21 +23,20 @@ module JekyllFeed
       # first archive feed.
       prev_archive = nil
 
-      per_page = 10
-      feed_posts = @site.posts.docs.reject {|d| d.draft?}
+      feed_posts = @site.posts.docs.reject(&:draft?)
 
       # Never include the most recent post in an archive feed. We'll have some
       # overlap between the last archive feed and the current feed, but there's
       # no point duplicating _all_ the posts in both places.
-      archive_page_count = (feed_posts.length - 1).div(per_page)
+      archive_page_count = (feed_posts.length - 1).div(PER_PAGE)
 
       dir = File.dirname(feed_path)
-      base = File.basename(feed_path, '.*')
+      base = File.basename(feed_path, ".*")
       ext = File.extname(feed_path)
 
       # Generate archive feeds first, starting from the oldest posts.
       1.upto(archive_page_count) do |pagenum|
-        posts = feed_posts[(pagenum - 1) * per_page, per_page].reverse
+        posts = feed_posts[(pagenum - 1) * PER_PAGE, PER_PAGE].reverse
 
         # If any of the posts in this page change, then we need to ensure that
         # RFC5005 consumers see the changes. Do this with the standard
@@ -49,14 +48,14 @@ module JekyllFeed
         page_path = File.join(dir, "#{base}-#{pagenum}-#{digest.hexdigest}#{ext}")
 
         page = PageWithoutAFile.new(@site, __dir__, "", page_path)
-        prev_archive = content_for_file(page, feed_source, posts, prev_archive, current)
+        prev_archive = content_for_file(page, posts, prev_archive, current)
         @site.pages << prev_archive
       end
 
       # Finally, generate the current feed. We can't do this earlier because we
       # have to compute the filename of the last archive feed first.
-      posts = feed_posts.reverse.take(per_page)
-      @site.pages << content_for_file(current, feed_source, posts, prev_archive, nil)
+      posts = feed_posts.reverse.take(PER_PAGE)
+      @site.pages << content_for_file(current, posts, prev_archive, nil)
     end
 
     private
@@ -66,6 +65,9 @@ module JekyllFeed
     #   2. A '}', which closes a Liquid tag
     # We will strip all of this whitespace to minify the template
     MINIFY_REGEX = %r!(?<=>|})\s+!
+
+    # Number of posts per feed
+    PER_PAGE = 10
 
     # Path to feed from config, or feed.xml for default
     def feed_path
@@ -94,7 +96,7 @@ module JekyllFeed
     def digest_posts(posts, prev_archive)
       digest = Digest::MD5.new
       posts.each do |post|
-        filtered = post.data.reject {|k, v| k == 'excerpt' || k == 'draft'}
+        filtered = post.data.reject { |k, _v| k == "excerpt" || k == "draft" }
         digest.file(post.path).update(filtered.to_s)
       end
       digest.update(prev_archive.url) unless prev_archive.nil?
@@ -102,8 +104,8 @@ module JekyllFeed
     end
 
     # Generates contents for a file
-    def content_for_file(file, file_source, posts, prev_archive, current)
-      file.content = file_source
+    def content_for_file(file, posts, prev_archive, current)
+      file.content = @feed_source
       file.data["layout"] = nil
       file.data["sitemap"] = false
       file.data["posts"] = posts
