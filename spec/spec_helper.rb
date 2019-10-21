8f9e10f38808c444b891a15a7c0f5046503204e2
Ashwin Maroli <ashmaroli@gmail.com>
2019-10-21 20:00:06 +0530

    Undo changes to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,16 +13,11 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.order = "random"
 
-  SOURCE_DIR = File.expand_path("../fixtures", __FILE__)
-  SOURCE_DIR2 = File.expand_path("../fixtures2", __FILE__)
-  DEST_DIR   = File.expand_path("../dest",     __FILE__)
+  SOURCE_DIR = File.expand_path("fixtures", __dir__)
+  DEST_DIR   = File.expand_path("dest",     __dir__)
 
   def source_dir(*files)
     File.join(SOURCE_DIR, *files)
-  end
-
-  def source_dir2(*files)
-    File.join(SOURCE_DIR2, *files)
   end
 
   def dest_dir(*files)
