d98228527573a286fe52964b2e1edb518ce111e2
Craig Stewart <craig@craig-james-stewart.co.uk>
2020-06-22 17:19:05 +0100

    remove test for core jekyll functionality suggested by parkr

--- spec/jekyll-feed_spec.rb
+++ spec/jekyll-feed_spec.rb
@@ -501,7 +501,6 @@ describe(JekyllFeed) do
     let(:tags_feed_test) { File.read(dest_dir("feed/by_tag/test.xml")) }
     let(:tags_feed_fail) { File.read(dest_dir("feed/by_tag/fail.xml")) }
     let(:tags_feed_success) { File.read(dest_dir("feed/by_tag/success.xml")) }
-    let(:tags_feed_nonarray) { File.read(dest_dir("feed/by_tag/nonarray.xml")) }
 
 
     context "do not set tags setting" do
@@ -630,24 +629,6 @@ describe(JekyllFeed) do
           expect(Pathname.new(dest_dir("questionable/path/success.xml"))).to exist
         end
       end
-    end
-
-    context "test nonarray tag value" do
-      let(:overrides) do
-        {
-          "feed" => {
-            "tags" => {
-              "only" => ["nonarray"]
-            },
-          },
-        }
-      end
-
-      it "should write nonarray feed" do
-        expect(Pathname.new(dest_dir("feed/by_tag/nonarray.xml"))).to exist
-        expect(tags_feed_nonarray).to match "/2015/05/12/liquid.html"
-      end
- 
     end
   end
 
