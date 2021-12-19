612d8e9201e209882d8f6b9ad7221551c589d81d
Ashwin Maroli <ashmaroli@gmail.com>
2021-12-19 21:04:35 +0530

    Test singular as well as plural front matter keys

--- spec/jekyll-feed_spec.rb
+++ spec/jekyll-feed_spec.rb
@@ -397,10 +397,11 @@ describe(JekyllFeed) do
     context "with top-level post categories" do
       let(:overrides) do
         {
-          "feed" => { "categories" => ["jekyll"] },
+          "feed" => { "categories" => %w(news jekyll) },
         }
       end
-      let(:category_feed) { File.read(dest_dir("feed/jekyll.xml")) }
+      let(:singular_category_feed) { File.read(dest_dir("feed/news.xml")) }
+      let(:plural_categories_feed) { File.read(dest_dir("feed/jekyll.xml")) }
 
       it "outputs the primary feed" do
         expect(contents).to match "http://example.org/updates/jekyll/2014/03/04/march-the-fourth.html"
@@ -410,10 +411,13 @@ describe(JekyllFeed) do
         expect(contents).to_not match "http://example.org/2016/02/09/a-draft.html"
       end
 
-      it "outputs the category feed" do
-        expect(category_feed).to match '<title type="html">My awesome site | News</title>'
-        expect(category_feed).to match "http://example.org/updates/jekyll/2014/03/04/march-the-fourth.html"
-        expect(category_feed).to match "March the fourth!"
+      it "outputs the category feeds" do
+        expect(singular_category_feed).to match '<title type="html">My awesome site | News</title>'
+        expect(singular_category_feed).to match "http://example.org/news/2014/03/02/march-the-second.html"
+        expect(singular_category_feed).to match "March the second!"
+        expect(plural_categories_feed).to match '<title type="html">My awesome site | News</title>'
+        expect(plural_categories_feed).to match "http://example.org/updates/jekyll/2014/03/04/march-the-fourth.html"
+        expect(plural_categories_feed).to match "March the fourth!"
       end
     end
 
